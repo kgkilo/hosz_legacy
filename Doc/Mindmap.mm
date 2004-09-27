6a8f2f47ea95594448a2b09b431d2cde69903314
kilo aka Gabor Kmetyko <kg.kilo@gmail.com>
2004-09-27 20:49:07 +0000

    MindMap dokumentacio

---
+++ Doc/Mindmap.mm
@@ -0,0 +1,61 @@
+<map version="0.7.1">
+<node TEXT="H&#x151;szolg" STYLE="fork">
+<node TEXT="Jogosults&#xe1;gi rendszer" POSITION="right">
+<node TEXT="Jogok &#xe1;br&#xe1;zol&#xe1;sa az adatb&#xe1;zisban">
+<node COLOR="#990000" TEXT="Jogok olvas&#xe1;sa az adatb&#xe1;zisb&#xf3;l">
+<icon BUILTIN="messagebox_warning"/>
+</node>
+<node COLOR="#000000" TEXT="Jogok &#xed;r&#xe1;sa az adatb&#xe1;zisba">
+<node TEXT="DOLGOZO t&#xe1;bla kezel&#xe9;se">
+<icon BUILTIN="button_ok"/>
+</node>
+<node TEXT="JOGOK t&#xe1;bla kezel&#xe9;se">
+<icon BUILTIN="xmag"/>
+</node>
+</node>
+</node>
+<node TEXT="Jogosults&#xe1;goknak megfelel&#x151; megjelen&#xed;t&#xe9;s">
+<node TEXT="Lek&#xe9;rdez&#x151; k&#xe9;perny&#x151;k"/>
+<node TEXT="Manipul&#xe1;tor k&#xe9;perny&#x151;k">
+<node TEXT="R&#xe9;gi rendszer">
+<node TEXT="1) no access">
+<node TEXT="Probl&#xe9;m&#xe1;s k&#xe9;perny&#x151;k"/>
+</node>
+<node TEXT="2) read access"/>
+<node TEXT="3) write access"/>
+</node>
+<node TEXT="Munkalap">
+<node TEXT="1) no access"/>
+<node TEXT="2) read access"/>
+<node TEXT="3) ki&#xe1;ll&#xed;that"/>
+<node TEXT="4) vissza&#xed;rhat"/>
+</node>
+<node TEXT="Lelt&#xe1;r"/>
+</node>
+</node>
+</node>
+<node TEXT="Lelt&#xe1;r" POSITION="left"/>
+<node TEXT="Egyedi jav&#xed;t&#xe1;sok" POSITION="right">
+<node TEXT="R&#xe9;gi">
+<node COLOR="#338800" TEXT="&#xda;j m&#x171;velet/t&#xed;pushiba k&#xf3;dok" FOLDED="true">
+<icon BUILTIN="button_ok"/>
+<node COLOR="#338800" TEXT="M&#x171;velet: Q298">
+<icon BUILTIN="button_ok"/>
+</node>
+<node COLOR="#338800" TEXT="T&#xed;pushiba: Q293">
+<icon BUILTIN="button_ok"/>
+</node>
+</node>
+</node>
+<node TEXT="MunkVB">
+<node COLOR="#338800" TEXT="Minden d&#xe1;tum mez&#x151; DTPicker legyen">
+<icon BUILTIN="button_ok"/>
+</node>
+<node COLOR="#338800" TEXT="Telep&#xed;t&#xe9;si helysz&#xe1;m combobox legyen">
+<icon BUILTIN="button_ok"/>
+</node>
+</node>
+<node TEXT="K&#xf6;z&#xf6;s"/>
+</node>
+</node>
+</map>
